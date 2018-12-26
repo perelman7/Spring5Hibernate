01d2435ef118291480f0ae52052ca2f3c569552b
Aleksei Saburov <asaburov@bostonsd.ru>
2018-12-26 19:18:52 +0300

    changed configuration gradle and added plugin "lombok"

--- src/main/java/com/hibernateTest/hibernateTest/controller/DepartmentController.java
+++ src/main/java/com/hibernateTest/hibernateTest/controller/DepartmentController.java
@@ -3,21 +3,20 @@ package com.hibernateTest.hibernateTest.controller;
 import com.hibernateTest.hibernateTest.model.Department;
 import com.hibernateTest.hibernateTest.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping(path = "/demo")
+@RestController
+@RequestMapping(path = "/deps")
 public class DepartmentController {
 
     @Autowired
     private DepartmentRepository repository;
 
     @GetMapping(path = "/add")
-    public @ResponseBody String addNewDepartment(@RequestParam String name, @RequestParam String description){
+    public String addNewDepartment(@RequestParam String name, @RequestParam String description){
         try {
             repository.save(new Department(name, description));
         }catch (Exception ex){}
@@ -26,7 +25,10 @@ public class DepartmentController {
     }
 
     @GetMapping(path = "/all")
-    public @ResponseBody Iterable<Department> getAllDepartments(){
+    public Iterable<Department> getAllDepartments(){
         return repository.findAll();
     }
+
+    @GetMapping(path = "/test")
+    public String test(){return "test";}
 }
--- src/main/java/com/hibernateTest/hibernateTest/model/Department.java
+++ src/main/java/com/hibernateTest/hibernateTest/model/Department.java
@@ -1,67 +1,33 @@
 package com.hibernateTest.hibernateTest.model;
 
+import lombok.*;
+
 import java.io.Serializable;
 import javax.persistence.*;
 
 @Entity
 @Table(name = "departments")
+@Data
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class Department implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
+    @Getter @Setter
     private int id;
 
     @Column(name = "name_dep")
+    @Getter @Setter
     private String name;
 
     @Column(name = "description")
+    @Getter @Setter
     private String description;
-
-    public Department() {
-    }
 
     public Department(String name, String description) {
         this.name = name;
         this.description = description;
-    }
-
-    public Department(int id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                '}';
     }
 }
