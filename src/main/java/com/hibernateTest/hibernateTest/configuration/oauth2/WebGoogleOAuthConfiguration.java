2b03524d86c81b2b53c40de8a9ea170e3753dd28
Aleksei Saburov <asaburov@bostonsd.ru>
2019-01-16 19:29:25 +0300

    no message

--- src/main/java/com/hibernateTest/hibernateTest/configuration/oauth2/WebGoogleOAuthConfiguration.java
+++ src/main/java/com/hibernateTest/hibernateTest/configuration/oauth2/WebGoogleOAuthConfiguration.java
@@ -32,7 +32,6 @@ public class WebGoogleOAuthConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                //.antMatcher("/**")
                 .authorizeRequests()
                 .antMatchers("/", "/login**", "/js/**", "/error**").permitAll()
                 .anyRequest().authenticated()
