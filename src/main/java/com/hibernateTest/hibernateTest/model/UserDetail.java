6fb47545b60613975580c6174b92baa51d8fa6d9
perelman7 <a.saburov737@gmail.com>
2019-01-25 21:06:31 +0300

    annd UserDetail model and controller for sing in.

---
+++ src/main/java/com/hibernateTest/hibernateTest/model/UserDetail.java
@@ -0,0 +1,11 @@
+package com.hibernateTest.hibernateTest.model;
+
+public class UserDetail {
+
+    private int id;
+    private String name;
+    private String swurname;
+    private String email;
+    private String role;
+    private String googleId;
+}
